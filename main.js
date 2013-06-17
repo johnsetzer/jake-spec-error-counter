3c1bdffaec4c6b93282174da8444891fb5b8a2f2
Jesse Martin <jesse.martin@ensighten.com>
2013-06-17 15:00:52 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var optimist = require('optimist');
 
 /**
  * Creates a jake task to display tests within the project that most frequently fail.
+ * This is a hack to make this an optional dependency for Jake.
  */
 var errorCounterFunc = function (jenkinsProject, argString) {
   var args = argString.split(' ');
